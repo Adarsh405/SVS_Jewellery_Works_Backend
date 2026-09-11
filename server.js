669b55f9aed8c07a43bc06724a6106bbc24e604a
Adarsh <125676861+Adarsh405@users.noreply.github.com>
2026-09-11 23:38:31 +0530

    Update CORS origin to production URL

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://svs-jewellery-works-frontend.vercel.app",
     credentials: true,
   })
 );
